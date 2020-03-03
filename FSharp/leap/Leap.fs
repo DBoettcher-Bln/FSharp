a11b2cf03266d04e53d9ef6f71eaf68aea044a0c
Dirk Boettcher <s_boettcherd@stud.hwr-berlin.de>
2020-03-03 19:59:59 +0100

    solved isogram

--- FSharp/leap/Leap.fs
+++ FSharp/leap/Leap.fs
@@ -6,16 +6,12 @@ let leapYear (year: int): bool =
     let mutable div4: int = 0
     let mutable div100: int = 0
     let mutable div400: int = 0
-
     div4 <- currYear % 4
-
     result <- if div4 = 0 then true else false
-
     if (result) then
         (div100 <- currYear % 100
          result <- if div100 = 0 then false else true
          if (not result) then
              (div400 <- currYear % 400
               result <- if div400 = 0 then true else false))
-
     result
